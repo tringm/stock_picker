e776bd8d202b4044be4461be274d87be07deebcc
Tri Nguyen <tringuyenminh23@gmail.com>
2020-03-01 13:22:45 +0200

    fix tests

--- tests/scrapper/test_macrotrends_scrapper.py
+++ tests/scrapper/test_macrotrends_scrapper.py
@@ -9,22 +9,22 @@ class TestMacrotrendsScrapper(TestCaseCompare):
         super().setUpClass()
         cls.scrapper = MacrotrendsScrapper()
 
-    def test_fetch_industry_listing_urls(self):
-        industry_listing = self.scrapper.fetch_industry_listing_urls()
+    def test_scrap_industry_listing_urls(self):
+        industry_listing = self.scrapper.scrap_industry_listing_urls()
         with self.exp_file_path.open() as f:
             exp_industry_listing = json.load(f)
         self.assertDictSubSet(industry_listing, exp_industry_listing)
 
-    def test_fetch_stocks_url_and_profile_in_industry(self):
+    def test_scrap_stocks_data_from_industry_listing(self):
         industry_listing_url = 'https://www.macrotrends.net/stocks/industry/89/'
-        stocks_url_and_profile = self.scrapper.fetch_stocks_url_and_profile_in_industry(industry_listing_url)
+        stocks_data = self.scrapper.scrap_stocks_data_from_industry_listing(industry_listing_url)
         with self.exp_file_path.open() as f:
-            exp_stocks_url_and_profile = json.load(f)
-        self.assertDictSubSet(stocks_url_and_profile, exp_stocks_url_and_profile)
+            exp_stocks_data = json.load(f)
+        self.assertDictSubSet(stocks_data, exp_stocks_data)
 
-    def test_fetch_stock_data(self):
+    def test_scrap_stock_data(self):
         stock_url = 'https://www.macrotrends.net/stocks/charts/BRK.B/berkshire-hathaway'
-        stock_data = self.scrapper.fetch_stock_data(stock_url)
+        stock_data = self.scrapper.scrap_stock_data(stock_url)
         with self.exp_file_path.open() as f:
             exp_stock_data = json.load(f)
         self.assertDictSubSet(stock_data, exp_stock_data)
