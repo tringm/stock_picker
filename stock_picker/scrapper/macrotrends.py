e5f0f15e0ac9cf0575e3b177dd659bf82f68a9ed
Tri Nguyen <tringuyenminh23@gmail.com>
2020-03-11 19:55:18 +0200

    async scrap stock data

--- stock_picker/scrapper/macrotrends.py
+++ stock_picker/scrapper/macrotrends.py
@@ -3,8 +3,9 @@ import requests
 import logging
 import re
 import json
-from typing import Dict
-
+from typing import Dict, List, Optional
+from aiohttp import ClientSession
+import asyncio
 
 LOG = logging.getLogger('MacrotrendsScrapper')
 
@@ -23,10 +24,10 @@ class FetchError(BaseError):
         super().__init__(f'failed to fetch {url}: {error}')
 
 
-class ParseError(BaseError):
-    def __init__(self, message, parsing_content):
-        LOG.debug(f'fail to parse: {parsing_content}')
-        super().__init__(message)
+class ScrapError(BaseError):
+    def __init__(self, error, parsing_content):
+        super().__init__(f'failed to scrap: {error}')
+        LOG.debug(f'failed scrap content: {parsing_content}')
 
 
 class MacrotrendsScrapper:
@@ -44,12 +45,27 @@ class MacrotrendsScrapper:
         return res.content
 
     @staticmethod
+    async def _async_fetch(client_session: ClientSession, url: str) -> Optional[str]:
+        """Asynchronously perform a get request
+
+        :param client_session: aiohttp Client Session
+        :param url: fetching url
+        :return: None if response status is not 200
+        """
+        LOG.debug(f'fetching {url}')
+        async with client_session.get(url) as resp:
+            if resp.status != 200:
+                return None
+            return await resp.text()
+
+    @staticmethod
     def _soupify(content, parser):
         """Raise error if cannot soupify content"""
         try:
             soup = BeautifulSoup(content, parser)
         except Exception as e:
-            raise ParseError(f'fail to soupify content with parser {parser}: {e}', content)
+            LOG.debug(f'failed soupify content: {content}')
+            raise BaseError(f'fail to soupify content with parser {parser}: {e}')
         return soup
 
     @staticmethod
@@ -57,7 +73,7 @@ class MacrotrendsScrapper:
         """Raise error if find no match when regex"""
         re_match = re.search(pattern, string)
         if not re_match:
-            raise ParseError(f'regex found no match with pattern {pattern}', string)
+            raise ScrapError(f'regex found no match with pattern {pattern}', string)
         return string[re_match.start():re_match.end()]
 
     @staticmethod
@@ -72,14 +88,14 @@ class MacrotrendsScrapper:
         soup = self._soupify(res, 'html.parser')
         tables = soup.find_all('table')
         if not tables:
-            raise ParseError('tables not found in industry listing page', soup)
+            raise ScrapError('tables not found in industry listing page', soup)
         list_by_industry_table = [tbl for tbl in tables if tbl.thead.tr.th.strong.string == 'Stocks by Industry']
         if not list_by_industry_table:
-            raise ParseError('table `Stocks by Industry` not found in industry listing page', tables)
+            raise ScrapError('table `Stocks by Industry` not found in industry listing page', tables)
         list_by_industry_table = list_by_industry_table[0]
         industry_cells = list_by_industry_table.find_all('td', attrs={'style': 'text-align:left'})
         if not industry_cells:
-            raise ParseError('no industry cells in table `Stock by Industry`', list_by_industry_table)
+            raise ScrapError('no industry cells in table `Stock by Industry`', list_by_industry_table)
         industry_listing_urls = {}
         parse_succeed = 0
         for cell in industry_cells:
@@ -102,12 +118,12 @@ class MacrotrendsScrapper:
         soup = self._soupify(res, 'html.parser')
         data_script = soup.find('script', string=re.compile('var data'))
         if not data_script:
-            raise ParseError('no script containing `var data`', soup)
+            raise ScrapError('no script containing `var data`', soup)
         stocks_data_table_string = self._regex_search_must_exist(r'(?<=var data = )(.*)', data_script.string)
         try:
             scrapped_stocks_data_table = json.loads(stocks_data_table_string)
         except Exception as e:
-            raise ParseError(f'fail to JSON parse stocks data table: {e}', stocks_data_table_string)
+            raise ScrapError(f'fail to JSON parse stocks data table: {e}', stocks_data_table_string)
         LOG.debug(f'scrapped stocks data table: {scrapped_stocks_data_table}')
         stocks_data = {}
         parse_succeed = 0
@@ -138,60 +154,75 @@ class MacrotrendsScrapper:
         LOG.info(f'successfully parsed {parse_succeed}/{len(stocks_data)} stocks data in this industry listing')
         return stocks_data
 
-    def scrap_stock_financial_aspect_data(self, financial_aspect: str, stock_url: str):
-        """Fetch a finanicla aspect data of a stock
+    def fetch_stock_detail_pages(self, stock_main_page_url: str) -> List:
+        """async fetch income statement, balance sheet, cash flow statement and price pages of a stock
 
-        :param financial_aspect:
-        :param stock_url:
+        :param stock_main_page_url: stock main page url to be used as prefix
+        :return: List of page content
+        """
+
+        async def run():
+            async with ClientSession(raise_for_status=True) as session:
+                tasks = [self._async_fetch(session, url) for url in detail_page_urls]
+                return await asyncio.gather(*tasks)
+
+        detail_page_urls = (
+            f'{stock_main_page_url}/{postfix}'
+            for postfix in ('income-statement', 'balance-sheet', 'cash-flow-statement', 'stock-price-history')
+        )
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        responses = loop.run_until_complete(run())
+        LOG.info(f'fetched details page of {stock_main_page_url}')
+        return responses
+
+    def scrap_stock_financial_aspect_page(self, financial_aspect_page_content: str):
+        """ scrap financial aspect data from page
+
+        :param financial_aspect_page_content:
         :return:
         """
-        financial_aspect_page_url = f'{stock_url}/{financial_aspect}'
-        LOG.debug(f'fetching {financial_aspect} data from {financial_aspect_page_url}...')
-        res = self._fetch(financial_aspect_page_url)
-        soup = self._soupify(res, 'html.parser')
+        soup = self._soupify(financial_aspect_page_content, 'html.parser')
         data_script = soup.find('script', string=re.compile('var originalData'))
         if not data_script:
-            raise ParseError('no script containing `var originalData` in the page content', soup)
+            raise ScrapError('no script containing `var originalData` in the page content', soup)
         raw_data_string = self._regex_search_must_exist(r'(?<=originalData = )(.*)', data_script.string)
         try:
             raw_data = json.loads(raw_data_string.strip()[:-1])  # strip space and remove last semi colon
         except Exception as e:
-            raise ParseError(f'failed to JSON parse stock data: {e}', {raw_data_string})
+            raise ScrapError(f'failed to JSON parse stock data: {e}', {raw_data_string})
         years = [key for key in raw_data[0].keys() if key not in ['field_name', 'popup_icon']]
-        stock_data = {
+        scrapped_data = {
             'years': years
         }
         for raw_field_data in raw_data:
             try:
                 field_name = self._beautify_field(self._soupify(raw_field_data['field_name'], 'html.parser').string)
             except Exception as e:
-                raise ParseError(f'failed to parse field name: {e}', raw_field_data)
+                raise ScrapError(f'table field name: {e}', raw_field_data)
             try:
-                stock_data[field_name] = [float(raw_field_data[yr]) if raw_field_data[yr] else None for yr in years]
+                scrapped_data[field_name] = [float(raw_field_data[yr]) if raw_field_data[yr] else None for yr in years]
             except Exception as e:
-                raise ParseError(f'failed to parse `{field_name}` data: {e}', raw_field_data)
-        LOG.info(f"fetched {financial_aspect} data from {financial_aspect_page_url}")
-        return stock_data
+                raise ScrapError(f'table `{field_name}` data: {e}', raw_field_data)
+        LOG.debug(f'scrapped financial aspect data from {financial_aspect_page_content}')
+        return scrapped_data
 
-    def scrap_stock_price_data_and_profile_data(self, stock_url: str) -> Dict:
-        """Fetch stock historical price data and industry, sector, and description info from its price page
+    def scrap_stock_price_data_and_profile_page(self, price_page_content: str) -> Dict:
+        """scrap stock historical price data and industry, sector, and description info from its price page
 
-        :param stock_url:
+        :param price_page_content:
         :return: Dictionary of price and other info
         """
-        stock_price_page_url = f'{stock_url}/stock-price-history'
-        LOG.debug(f'fetching price data from {stock_price_page_url}...')
-        res = self._fetch(stock_price_page_url)
-        soup = self._soupify(res, 'html.parser')
+        soup = self._soupify(price_page_content, 'html.parser')
         try:
             price_table_element, profile_table_element, _ = soup.find_all('table', class_='historical_data_table')
         except Exception as e:
-            raise ParseError(f'fail to parse page into price table and info table: {e}', soup)
+            raise ScrapError(f'price table and info table: {e}', soup)
         try:
             price_table_headers = [self._beautify_field(th.string)
                                    for th in price_table_element.find_all('th') if 'colspan' not in th.attrs]
         except Exception as e:
-            raise ParseError(f'fail to parse price table header: {e}', price_table_element)
+            raise ScrapError(f'price table header: {e}', price_table_element)
         price_table_row_elements = price_table_element.tbody.find_all('tr')
         price_data = {tbl_header: [] for tbl_header in price_table_headers}
         for tbl_row_element in price_table_row_elements:
@@ -203,29 +234,29 @@ class MacrotrendsScrapper:
                 price_data[price_table_headers[idx]].append(cell_data)
         price_data['years'] = price_data['year']
         price_data.pop('year', None)
-        stock_data = {'price': price_data}
+        scrapped_data = {'price': price_data}
         try:
             profile_table_headers = [th.string for th in profile_table_element.find_all('th')]
         except Exception as e:
             LOG.error(f'fail to parse info table header: {e}')
-            return stock_data
+            return scrapped_data
         profile_table_data_elements = [td for td in profile_table_element.find_all('td') if 'colspan' not in td.attrs]
         try:
-            stock_data['sector'] = self._beautify_field(
+            scrapped_data['sector'] = self._beautify_field(
                 profile_table_data_elements[profile_table_headers.index('Sector')].string)
         except Exception as e:
-            raise ParseError(f'fail to parse sector: {e}', profile_table_data_elements)
+            raise ScrapError(f'sector: {e}', profile_table_data_elements)
         try:
-            stock_data['industry'] = self._beautify_field(
+            scrapped_data['industry'] = self._beautify_field(
                 profile_table_data_elements[profile_table_headers.index('Industry')].string)
         except Exception as e:
-            raise ParseError(f'fail to parse industry: {e}', profile_table_data_elements)
+            raise ScrapError(f'industry: {e}', profile_table_data_elements)
         try:
-            stock_data['description'] = profile_table_element.find('td', attrs={'colspan': 4}).span.string
+            scrapped_data['description'] = profile_table_element.find('td', attrs={'colspan': 4}).span.string
         except Exception as e:
-            raise ParseError(f'fail to parse description: {e}', profile_table_element)
-        LOG.info(f'fetched price data from {stock_price_page_url}')
-        return stock_data
+            raise ScrapError(f'description: {e}', profile_table_element)
+        LOG.debug(f'scrapped price data')
+        return scrapped_data
 
     def scrap_stock_data(self, stock_url: str):
         """Scrap stock data from its url
@@ -234,9 +265,12 @@ class MacrotrendsScrapper:
         :return:
         """
         LOG.debug(f'fetching stock data of {stock_url}...')
-        stock_data = {}
-        for fin_aspect in ('income-statement', 'balance-sheet', 'cash-flow-statement'):
-            stock_data[self._beautify_field(fin_aspect)] = self.scrap_stock_financial_aspect_data(fin_aspect, stock_url)
-        stock_data.update(self.scrap_stock_price_data_and_profile_data(stock_url))
-        LOG.debug(f'fetched stock data of {stock_url}')
+        responses = self.fetch_stock_detail_pages(stock_url)
+        stock_data = {
+            'income_statement': self.scrap_stock_financial_aspect_page(responses[0]),
+            'balance_sheet': self.scrap_stock_financial_aspect_page(responses[1]),
+            'cash_flow_statement': self.scrap_stock_financial_aspect_page(responses[2]),
+            'stock_price_history': self.scrap_stock_price_data_and_profile_page(responses[3])
+        }
+        LOG.info(f'fetched stock data of {stock_url}')
         return stock_data
